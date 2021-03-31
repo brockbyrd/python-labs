2892a2a88d0614e49b2597bf97a987c6af890a23
brockbyrd <brockbyrdd@gmail.com>
2021-03-31 10:41:31 -0500

    small lesson intro to lists and grabbing items from index

---
+++ lists.py
@@ -0,0 +1,8 @@
+friends = ["Dustin", "Sam", "Cole", "Alex"]
+
+print(friends[3])
+
+#grabs elements after index of 1
+print(friends[1:])
+#grabs elements after index of 1 up to 2
+print(friends[1:3])
