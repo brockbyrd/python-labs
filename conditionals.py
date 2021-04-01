0adc4b924d298cb0730a9773c75a35b59cbb3005
brockbyrd <brockbyrdd@gmail.com>
2021-04-01 14:08:00 -0500

    creates power function with for loop

--- conditionals.py
+++ conditionals.py
@@ -65,3 +65,12 @@ for index in range(5):
         print("First iteration")
     else:
         print("Not first")
+
+#exponent function
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for index in range(pow_num):
+        result = result * base_num
+    return result
+
+print(raise_to_power(3, 3))
