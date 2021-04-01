63d9b8510c54f1e921323413d6273736c5fa16aa
brockbyrd <brockbyrdd@gmail.com>
2021-04-01 13:27:32 -0500

    adds for loop examples to conditionals

--- conditionals.py
+++ conditionals.py
@@ -44,3 +44,24 @@ while i <= 10:
     print(i)
     i += 1
 print("Done with while loop")
+
+
+for letter in "Flatiron School":
+    print(letter)
+
+
+friends = ["Dustin", "Sam", "Cole", "Alex"]
+for friend in friends:
+    print(friend)
+
+for index in range(3, 10):
+    print(index)
+
+for index in range(len(friends)):
+    print(friends[index])
+
+for index in range(5):
+    if index == 0:
+        print("First iteration")
+    else:
+        print("Not first")
