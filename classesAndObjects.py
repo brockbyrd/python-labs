66410e7923d149b85a7d45a204846fb7f548ec87
brockbyrd <brockbyrdd@gmail.com>
2021-04-02 11:24:45 -0500

    creates a class function for student

--- classesAndObjects.py
+++ classesAndObjects.py
@@ -1,9 +1,16 @@
 from Student import Student
 
 student1 = Student("Brock", "Computer Science", 3.2, False)
+student2 = Student("Alex", "Interior Design", 3.9, False)
 
 print(student1)
 print(student1.name)
 print(student1.major)
 print(student1.gpa)
 print(student1.is_on_probation)
+print(student1.on_honor_roll())
+print(student2.name)
+print(student2.major)
+print(student2.gpa)
+print(student2.is_on_probation)
+print(student2.on_honor_roll())
