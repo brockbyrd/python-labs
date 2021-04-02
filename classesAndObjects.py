6ce1b5a0a97ecf6b88ae4df39eb57effc9b682b3
brockbyrd <brockbyrdd@gmail.com>
2021-04-02 10:50:19 -0500

    shows classes with Student.py and classesAndObjects

---
+++ classesAndObjects.py
@@ -0,0 +1,9 @@
+from Student import Student
+
+student1 = Student("Brock", "Computer Science", 3.2, False)
+
+print(student1)
+print(student1.name)
+print(student1.major)
+print(student1.gpa)
+print(student1.is_on_probation)
