4fe720784825b414ee80730d14313bc318ce41c3
brockbyrd <brockbyrdd@gmail.com>
2021-05-11 15:03:50 -0500

    sets background to white and draws circle in middle

--- basicPyGame.py
+++ basicPyGame.py
@@ -8,3 +8,13 @@ screen = pygame.display.set_mode([500, 500])
 #run until user asks to quit
 running = True
 while running:
+
+    #Did the user click the window close button?
+    for event in pygame.event.get():
+        running = False
+
+    #Fill the background with white
+    screen.fill((255, 255, 255))
+
+    #draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
