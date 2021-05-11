a8dcd651e989d4c32e80ee87753f0676f0f7717b
brockbyrd <brockbyrdd@gmail.com>
2021-05-11 15:01:34 -0500

    sets screen window size and running program set true

--- basicPyGame.py
+++ basicPyGame.py
@@ -1,2 +1,10 @@
+#import and inits the pygame library
 import pygame
 pygame.init()
+
+#set up drawing window
+screen = pygame.display.set_mode([500, 500])
+
+#run until user asks to quit
+running = True
+while running:
