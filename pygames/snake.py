cbe0e228a32f8daae710b8107a2d41780c8195aa
Brock Byrd <brockbyrdd@gmail.com>
2021-06-02 15:30:14 -0500

    creates while loop to keep display window open

--- pygames/snake.py
+++ pygames/snake.py
@@ -6,6 +6,13 @@ display = pygame.dipslay.set_mode((400, 300))
 
 pygame.display.update()
 
+pygame.display.set_caption("Snake game")
+game_over = False
+
+while not game_over:
+    for event in pygame.event.get():
+        print(event)
+
 
 pygame.quit()
 quit()
