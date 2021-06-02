4aace67d53ddcca21c0de1dd8cd0f36572628259
Brock Byrd <brockbyrdd@gmail.com>
2021-06-02 16:01:35 -0500

    takes in keypresses and changes x and y position on keypress

--- pygames/snake.py
+++ pygames/snake.py
@@ -1,19 +1,47 @@
 import pygame
 pygame.init()
-display = pygame.display.set_mode((400, 300))
 
-pygame.display.set_caption("Snake game")
-
-blue=(0,0,255)
+white=(255,255,255)
+black=(0,0,0)
 red=(255,0,0)
 
+display = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Snake game")
+
 game_over = False
+
+x1 = 300
+y1 = 300
+
+x1_change = 0
+y1_change = 0
+
+clock = pygame.time.Clock()
+
 while not game_over:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             game_over=True
-    pygame.draw.rect(display, blue, [200,150,10,10])
+        if event.type==pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -10
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x1_change = 10
+                y1_change = 0
+            elif event.key == pygame.K_UP:
+                x1_change = 0
+                y1_change = -10
+            elif event.key == pygame.K_DOWN:
+                x1_change = 0
+                y1_change = 10
+    
+    x1 += x1_change
+    y1 += y1_change
+    display.fill(white)
+    pygame.draw.rect(display, black, [x1,y1,10,10])
     pygame.display.update()
+    clock.tick(30)
 
 
 pygame.quit()
