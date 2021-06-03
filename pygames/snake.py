3bc4ad4d18a908f2f7e2f66a13ea8d98f64e4ea0
Brock Byrd <brockbyrdd@gmail.com>
2021-06-03 12:45:31 -0500

    checks if game has been closed or not

--- pygames/snake.py
+++ pygames/snake.py
@@ -37,22 +37,36 @@ def gameLoop():
     foodx = round(random.randrange(0, display_width - snake_block) / 10.0) * 10.0
     foody = round(random.randrange(0, display_width - snake_block) / 10.0) * 10.0
     while not game_over:
+
+        while game_close == True:
+            display.fill(white)
+            message("You Lost! Press Q-Quit or C-Play Again", red)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 game_over=True
             if event.type==pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    x1_change = -10
+                    x1_change = -snake_block
                     y1_change = 0
                 elif event.key == pygame.K_RIGHT:
-                    x1_change = 10
+                    x1_change = snake_block
                     y1_change = 0
                 elif event.key == pygame.K_UP:
                     x1_change = 0
-                    y1_change = -10
+                    y1_change = -snake_block
                 elif event.key == pygame.K_DOWN:
                     x1_change = 0
-                    y1_change = 10
+                    y1_change = snake_block
         
         if x1 > display_width or x1 < 0 or y1 >= display_height or y1 < 0:
             game_over = True
